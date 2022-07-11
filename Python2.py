e2e510862867204b4604e4dfe87b8fae797a67ac
neha232419 <98411457+neha232419@users.noreply.github.com>
2022-07-11 21:51:14 +0530

    Update Python2.py

--- Python2.py
+++ Python2.py
@@ -1,2 +1,11 @@
-for x in range(0,100):
-    print(x)
+list1 = [12, -7, 5, 64, -14]
+for num in list1:  
+    if num >= 0:
+       print(num, end = " ")
+
+list2 = [12, 14, -95, 3]
+def loop(x):
+    for x in list2:
+        if x > 0:
+            print(x)
+loop(list2)
